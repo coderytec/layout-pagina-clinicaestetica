c5ff9e62a9f718244a094d4643529b5da0ea4460
CODERY.TEC <coderytec@gmail.com>
2026-09-21 18:45:59 -0300

    fix: ajuste icone

--- src/components/Modules/Modules.tsx
+++ src/components/Modules/Modules.tsx
@@ -4,7 +4,7 @@ import "./modules.css";
 export const Modules: React.FC = () => {
   const treatments = [
     {
-      icon: "fa-face-sparkles",
+      icon: "fa-solid fa-sparkles",
       category: "Facial Premium",
       title: "Harmonização Facial",
       desc: "Técnicas avançadas e seguras para restaurar proporções, volume e jovialidade com extrema naturalidade e sofisticação."
